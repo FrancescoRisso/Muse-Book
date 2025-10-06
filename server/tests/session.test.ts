d9e2a1d8d94e0047b6056673bb039125e3198ff0
FrancescoRisso <francesco@frisso.net>
2025-10-06 19:01:26 +0200

    Session tests

--- server/tests/session.test.ts
+++ server/tests/session.test.ts
@@ -1,28 +1,140 @@
 import { describe, test, expect, beforeEach } from "@jest/globals";
 import request from "supertest";
-import { clearDB } from "./utils";
-import db from "../db/db";
-import { createHash } from "crypto";
-
-const testPwd = "pwd";
-const testSalt = 123456789;
-const testHash = createHash("sha256").update(`${testPwd}${testSalt}`).digest("hex");
-
-const sessionDb = `
-INSERT INTO USER(Username, Salt, Hash, Email, Name, Surname, Language)
-VALUES("User123", ${testSalt}, "${testHash}", "user@name.com", "Mario", "Rossi", "IT");
-`;
+import { clearDB, insertDefaultUser, login, query_db } from "./utils";
+import { app } from "../index";
 
 beforeEach(() => {
-	clearDB()
-		// .then(() => db.run(sessionDb))
-		.catch((err) => console.error(err));
+	return new Promise<void>((resolve, reject) => {
+		clearDB()
+			.then(() => insertDefaultUser().then(resolve).catch(reject))
+			.catch(reject);
+	});
 });
 
 const baseUrl = "/musebook/api/session";
 
-describe("Access APIs", () => {
-	describe('"POST /"', () => {
-		test("", async () => {});
+describe(`Access APIs ("${baseUrl}")`, () => {
+	describe('Login ("POST /")', () => {
+		test("Successful login", async () => {
+			const user = { username: "User123", password: "pwd" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(200);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(1);
+		});
+
+		test("Successful login on another device", async () => {
+			login();
+			login();
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(2);
+		});
+
+		test("Missing username", async () => {
+			const user = { password: "pwd" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(422);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Missing password", async () => {
+			const user = { username: "User123" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(422);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Empty username", async () => {
+			const user = { username: "User123", password: "" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(422);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Empty password", async () => {
+			const user = { username: "", password: "pwd" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(422);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Wrong password", async () => {
+			const user = { username: "User123", password: "wrong" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(401);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Non-existing username", async () => {
+			const user = { username: "User456", password: "pwd" };
+			const res = request(app).post(baseUrl).send(user);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(401);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+	});
+
+	describe('Logout ("DELETE /")', () => {
+		test("Successful logout", async () => {
+			const cookie = await login();
+
+			const res = request(app).delete(baseUrl).set("Cookie", cookie);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(200);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
+
+		test("Successful logout with 2 logged devices", async () => {
+			const cookie1 = await login();
+			const cookie2 = await login();
+
+			const res = request(app).delete(baseUrl).set("Cookie", cookie2);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(200);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(1);
+		});
+
+		test("Logout of not logged in user", async () => {
+			const res = request(app).delete(baseUrl);
+
+			await expect(res).resolves.toBeDefined();
+			expect((await res).status).toBe(401);
+
+			const logged_devices = (await query_db("SELECT COUNT(*) FROM LOGGED_IN_DEVICES", []))[0]["COUNT(*)"];
+			expect(logged_devices).toBe(0);
+		});
 	});
 });
--- server/tests/utils.ts
+++ server/tests/utils.ts
@@ -1,5 +1,8 @@
+import { createHash } from "node:crypto";
 import db from "../db/db";
 import fs from "node:fs";
+import { app } from "..";
+import request from "supertest";
 
 const clearDBquery = fs.readFileSync("db/db-creation-script.sqlite3", "utf8");
 
@@ -8,5 +11,42 @@ export const clearDB = () => {
 		db.exec(clearDBquery, (err: any) => {
 			return err ? rej() : res();
 		});
+	});
+};
+
+export const query_db = (query: string, params: any[]): Promise<any[]> => {
+	return new Promise<any[]>((resolve, reject) => {
+		db.all(query, params, (err, res) => {
+			if (err) reject(err);
+			else resolve(res);
+		});
+	});
+};
+
+export const insertDefaultUser = async () => {
+	const testPwd = "pwd";
+	const testSalt = 123456789;
+	const testHash = createHash("sha256").update(`${testPwd}${testSalt}`).digest("hex");
+
+	const sessionDb = `
+	INSERT INTO USER(Username, Salt, Hash, Email, Name, Surname, Language, OfflineData)
+	VALUES("User123", ${testSalt}, "${testHash}", "user@name.com", "Mario", "Rossi", "IT", TRUE);
+	`;
+
+	return new Promise<void>((resolve, reject) => {
+		db.run(sessionDb, [], (err) => (err ? reject(err) : resolve()));
+	});
+};
+
+export const login = async (): Promise<string> => {
+	return new Promise<string>((resolve, reject) => {
+		request(app)
+			.post(`/ezelectronics/sessions`)
+			.send({ username: "User123", password: "pwd" })
+			.expect(200)
+			.end((err, res) => {
+				if (err) reject(err);
+				else resolve(res.header["set-cookie"][0]);
+			});
 	});
 };
